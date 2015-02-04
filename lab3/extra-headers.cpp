8f4ced06d38d2ce759f5b2986c21839c422b8726
prateekchandan <prateekchandan5545@gmail.com>
2015-02-04 15:30:15 +0530

    base classes added

--- lab3/extra-headers.cpp
+++ lab3/extra-headers.cpp
@@ -1,1 +1,43 @@
-int node_count=0;
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+struct typeExp
+{
+	string s;
+	typeExp(){};
+	typeExp(string s1)
+	{
+		s=s1;
+	}
+};
+
+class abstract_astnode
+{
+	public:
+		virtual void print () = 0;
+		virtual std::string generate_code(const symbolTable&) = 0;
+		virtual basic_types getType() = 0;
+		virtual bool checkTypeofAST() = 0;
+	protected:
+		virtual void setType(basic_types) = 0;
+	private:
+		typeExp astnode_type;
+};
+
+class stmtAst : public abstract_astnode{
+	public:
+		vector<abstract_astnode*> v;
+};
+
+class expAst : public abstract_astnode{
+	public:
+		vector<abstract_astnode*> v;
+
+};
+
+class arrayRef : public abstract_astnode{
+	public:
+		vector<abstract_astnode*> v;
+};
